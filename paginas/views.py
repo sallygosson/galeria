b60fbfaf2fa6a232620a2e7a5f97152d85865375
sallygosson <raagosson@gmail.com>
2025-04-10 11:20:13 -0300

    projeto pronto

--- paginas/views.py
+++ paginas/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def homepage(request):
+    return HttpResponse("<h1>Bem-vindo à Homepage!</h1>")
+
+def about(request):
+    return HttpResponse("<h1>Sobre nós</h1><p>Esta é a página About.</p>")
